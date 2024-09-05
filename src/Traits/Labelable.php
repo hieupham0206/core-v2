2cf7b7e8d4603c841f5c010f6fd226c50c5df8e1
Hieu Pham <hieu.pham@cloudteam.vn>
2024-09-05 16:23:10 +0700

    feat: đồng bộ tên hiển thi của Model

--- src/Traits/Labelable.php
+++ src/Traits/Labelable.php
@@ -2,44 +2,11 @@
 
 namespace Cloudteam\CoreV2\Traits;
 
-use Illuminate\Support\Str;
-use ReflectionClass;
-use ReflectionException;
-use function get_class;
-
 trait Labelable
 {
-    public function classLabel($lcfirst = false)
-    {
-        try {
-            $reflect = new ReflectionClass($this);
-
-            if (property_exists(get_class($this), 'logName')) {
-                //$nameInModel = $reflect->getStaticPropertyValue('logName');
-                $nameInModel = $reflect->getProperty('logName')->getValue($this);
-                $tableName   = __($nameInModel);
-
-                if (is_array($tableName)) {
-                    $tableName = $nameInModel;
-                }
-
-                return $lcfirst ? mb_strtolower($tableName) : $tableName;
-            }
-
-            return __(ucfirst(camel2words(Str::studly($reflect->getShortName()))));
-        } catch (ReflectionException $e) {
-            return '';
-        }
-    }
-
     public function badgeLabel($text, $context = 'success', $customClass = '', $size = ''): string
     {
         return "<span class='fw-bold badge badge-$context' $customClass $size>$text</span>";
-    }
-
-    public function getModelTitleAttribute(): ?string
-    {
-        return $this->classLabel(true);
     }
 
     public function getCreatedAtTextAttribute(): ?string
